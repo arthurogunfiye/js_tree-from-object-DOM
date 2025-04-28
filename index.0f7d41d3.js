4daccf151d1795e4d17b0a07874d953c7f1f9372
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-28 16:01:59 +0100

    make build

---
+++ index.0f7d41d3.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var l=document.createElement("li");l.innerText=a,Object.keys(r[a]).length>0&&e(l,r[a]),t.appendChild(l)}n.appendChild(t)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.0f7d41d3.js.map
